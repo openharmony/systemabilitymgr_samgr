3d7ee6e771eba0cd36cc909782ff057ee17c945e
huangxiaolinabc123 <huangxiaolin12@huawei.com>
2023-04-07 22:03:50 +0800

    add rust demo
    Signed-off-by: huangxiaolinabc123 <huangxiaolin12@huawei.com>
    Change-Id: Ife38135ced2aa831ce40030a1a989dde51134c6c

--- services/samgr/native/test/unittest/rust/rust_samgr_test.rs
+++ services/samgr/native/test/unittest/rust/rust_samgr_test.rs
@@ -15,10 +15,10 @@
 
 
 extern crate ipc_rust;
-extern crate sa_test;
+extern crate audio_rust_ipc;
 extern crate rust_samgr;
 extern crate test_access_token;
-extern crate rust_service_test;
+extern crate audio_rust_sa;
 use ipc_rust::{
     IRemoteBroker, RemoteObjRef
 };
@@ -30,15 +30,15 @@ const LOG_LABEL: HiLogLabel = HiLogLabel {
     tag: "rustSA"
 };
 use rust_samgr::{get_systemability_manager, SAExtraProp, get_service_proxy};
-use rust_service_test::{ TEST_SERVICE_ID, TestService };
+use audio_rust_sa::{ TEST_AUDIO_ID, TestService };
 use test_access_token::{ init_access_token };
-use sa_test::{
+use audio_rust_ipc::{
     ITest, TestStub
 };
 
 fn get_test_service() -> RemoteObjRef<dyn ITest>
 {
-    let object = get_service_proxy::<dyn ITest>(TEST_SERVICE_ID);
+    let object = get_service_proxy::<dyn ITest>(TEST_AUDIO_ID);
     let object = match object {
         Ok(object) => object,
         Err(error) => {
@@ -61,7 +61,18 @@ fn add_systemability_test()
     let service = TestStub::new_remote_stub(TestService).expect("create TestService failed");
     let sapro = SAExtraProp::default();
     let samgr_proxy = get_systemability_manager();
-    samgr_proxy.add_systemability(&service.as_object().unwrap(), TEST_SERVICE_ID, sapro).expect("add_systemability failed"); 
+    samgr_proxy.add_systemability(&service.as_object().unwrap(), TEST_AUDIO_ID, sapro).expect("add_systemability failed"); 
+}
+
+#[test]
+fn unload_systemability_test()
+{
+    init_access_token();
+    let service = TestStub::new_remote_stub(TestService).expect("create TestService failed");
+    let sapro = SAExtraProp::default();
+    let samgr_proxy = get_systemability_manager();
+    samgr_proxy.add_systemability(&service.as_object().unwrap(), TEST_AUDIO_ID, sapro).expect("add_systemability failed");
+    samgr_proxy.unload_systemability(TEST_AUDIO_ID).expect("unload_systemability failed");
 }
 
 #[test]
@@ -71,20 +82,8 @@ fn get_service_test()
     let service = TestStub::new_remote_stub(TestService).expect("create TestService failed");
     let sapro = SAExtraProp::default();
     let samgr_proxy = get_systemability_manager();
-    samgr_proxy.add_systemability(&service.as_object().unwrap(), TEST_SERVICE_ID, sapro).expect("add_systemability failed");
+    samgr_proxy.add_systemability(&service.as_object().unwrap(), TEST_AUDIO_ID, sapro).expect("add_systemability failed");
     get_test_service();
-}
-
-#[test]
-fn test_echo_str() {
-    init_access_token();
-    let service = TestStub::new_remote_stub(TestService).expect("create TestService failed");
-    let sapro = SAExtraProp::default();
-    let samgr_proxy = get_systemability_manager();
-    samgr_proxy.add_systemability(&service.as_object().unwrap(), TEST_SERVICE_ID, sapro).expect("add_systemability failed");
-    let remote = get_test_service();
-    let value = remote.echo_str("hello").expect("should return string");
-    assert_eq!(value, "hello");
 }
 
 #[test]
@@ -93,7 +92,7 @@ fn test_request_concurent() {
     let service = TestStub::new_remote_stub(TestService).expect("create TestService failed");
     let sapro = SAExtraProp::default();
     let samgr_proxy = get_systemability_manager();
-    samgr_proxy.add_systemability(&service.as_object().unwrap(), TEST_SERVICE_ID, sapro).expect("add_systemability failed");
+    samgr_proxy.add_systemability(&service.as_object().unwrap(), TEST_AUDIO_ID, sapro).expect("add_systemability failed");
     let remote = get_test_service();
     let value = remote.request_concurent(false).expect("should return true");
     assert!(value);
