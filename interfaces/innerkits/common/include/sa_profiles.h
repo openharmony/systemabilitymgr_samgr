6538d090bf9baae90c4faa7f684135894d6b54b3
huangxiaolinabc123 <huangxiaolin12@huawei.com>
2023-05-04 07:34:11 +0000

    update interfaces/innerkits/common/include/sa_profiles.h.
    Signed-off-by: huangxiaolinabc123 <huangxiaolin12@huawei.com>

--- interfaces/innerkits/common/include/sa_profiles.h
+++ interfaces/innerkits/common/include/sa_profiles.h
@@ -90,6 +90,7 @@ struct SaProfile {
     int32_t dumpLevel = 0;
     std::u16string capability;
     std::u16string permission;
+    // default OTHER_START
     uint32_t bootPhase = 3;
     StartOnDemand startOnDemand;
     StopOnDemand stopOnDemand;
