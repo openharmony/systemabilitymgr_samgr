686cc37f729a672f613cda21115be9547d80684d
huangjiabao <huangjiabao4@huawei.com>
2024-05-13 09:21:21 +0800

    commit msg
    Signed-off-by: huangjiabao <huangjiabao4@huawei.com>

--- services/samgr/native/source/schedule/system_ability_state_scheduler.cpp
+++ services/samgr/native/source/schedule/system_ability_state_scheduler.cpp
@@ -1285,7 +1285,7 @@ int32_t SystemAbilityStateScheduler::CheckStopEnableOnce(const OnDemandEvent& ev
             saControl.saId, event.eventId);
     }
     if (result != ERR_OK){
-        ReportSamgrSaLoadFail(saControl.saId, "ondemand load err:" + ToString(result));
+        ReportSaUnLoadFail(saControl.saId, "ondemand unload err:" + ToString(result));
     }
     return result;
 }
