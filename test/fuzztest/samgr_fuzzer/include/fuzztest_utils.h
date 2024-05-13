633698d0831899b887e3e25c5e52bd6a5bd6ee2d
gongguilin <gongguilin@h-partners.com>
2024-05-13 15:51:06 +0800

    add fuzz test
    Signed-off-by: gongguilin <gongguilin@h-partners.com>

---
+++ test/fuzztest/samgr_fuzzer/include/fuzztest_utils.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef SAMGR_TEST_FUZZTEST_SYSTEM_ABILITY_MANAGER_FUZZER_UTILS_H
+#define SAMGR_TEST_FUZZTEST_SYSTEM_ABILITY_MANAGER_FUZZER_UTILS_H
+#include <cstddef>
+#include <cstdint>
+namespace OHOS {
+namespace Samgr {
+class FuzzTestUtils {
+public:
+    static void FuzzTestRemoteRequest(const uint8_t *rawData, size_t size, uint32_t code);
+private:
+    static bool IsDmReady();
+    static void AddDeviceManager();
+};
+}
+} // namespace OHOS
+
+#endif
