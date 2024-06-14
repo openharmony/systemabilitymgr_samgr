82c69cfaee19d119c7af650663141199e5c62f56
chenbingbing <chenbingbing12@huawei.com>
2024-06-14 10:47:08 +0800

    deviceonline trigger condition rectification
    Signed-off-by: chenbingbing <chenbingbing12@huawei.com>

--- services/samgr/native/include/collect/device_networking_collect.h
+++ services/samgr/native/include/collect/device_networking_collect.h
@@ -38,7 +38,6 @@ class DeviceStateCallback : public DistributedHardware::DeviceStateCallback {
     private:
        sptr<ICollectPlugin> collect_;
        std::set<std::string> deviceOnlineSet_;
-       bool isExistDeviceReady_ = false;
        std::mutex deviceOnlineLock_;
 };
 
