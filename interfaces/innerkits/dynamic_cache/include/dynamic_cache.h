ea003ae0551990cd7fdab527dfcf23165b5f2a8a
wuminjie <wuminjie2@h-partners.com>
2024-07-08 17:18:55 +0800

    clearcache绕过cfi
    Signed-off-by: wuminjie <wuminjie2@h-partners.com>

--- interfaces/innerkits/dynamic_cache/include/dynamic_cache.h
+++ interfaces/innerkits/dynamic_cache/include/dynamic_cache.h
@@ -65,7 +65,7 @@ public:
         }
         return res;
     }
-
+    __attribute__((no_sanitize("cfi")))
     void ClearCache()
     {
         std::lock_guard<std::mutex> autoLock(queryCacheLock_);
