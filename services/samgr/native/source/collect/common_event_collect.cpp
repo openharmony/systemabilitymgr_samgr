421e0df63a2ebb264c023bb741b33e7ad4435b0e
龚桂林 <gongguilin@h-partners.com>
2024-10-11 07:49:10 +0000

    update services/samgr/native/source/collect/common_event_collect.cpp.
    Signed-off-by: 龚桂林 <gongguilin@h-partners.com>

--- services/samgr/native/source/collect/common_event_collect.cpp
+++ services/samgr/native/source/collect/common_event_collect.cpp
@@ -336,13 +336,13 @@ int64_t CommonEventCollect::SaveOnDemandReasonExtraData(const EventFwk::CommonEv
     wantMap[UID] = std::to_string(uid);
     wantMap[NET_TYPE] = std::to_string(netType);
     wantMap[BUNDLE_NAME] = want.GetBundle();
+    std::lock_guard<std::mutex> autoLock(extraDataLock_);
     for (auto key : extraDataKey_[want.GetAction()]) {
         wantMap[key] = GetParamFromWant(key, want);
     }
     wantMap[COMMON_EVENT_ACTION_NAME] = want.GetAction();
     OnDemandReasonExtraData extraData(data.GetCode(), data.GetData(), wantMap);
 
-    std::lock_guard<std::mutex> autoLock(extraDataLock_);
     int64_t extraDataId = GenerateExtraDataIdLocked();
     extraDatas_[extraDataId] = extraData;
     HILOGI("CommonEventCollect save extraData %{public}d", static_cast<int32_t>(extraDataId));
