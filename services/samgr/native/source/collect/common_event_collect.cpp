0cb392dd2d94cfcec9394f8c139cde35b7ce40f6
黄嘉宝 <huangjiabao4@huawei.com>
2025-02-15 15:39:07 +0800

    commit msg
    Signed-off-by: 黄嘉宝 <huangjiabao4@huawei.com>

--- services/samgr/native/source/collect/common_event_collect.cpp
+++ services/samgr/native/source/collect/common_event_collect.cpp
@@ -357,14 +357,17 @@ int64_t CommonEventCollect::SaveOnDemandReasonExtraData(const EventFwk::CommonEv
     wantMap[UID] = std::to_string(uid);
     wantMap[NET_TYPE] = std::to_string(netType);
     wantMap[BUNDLE_NAME] = want.GetBundle();
-    std::lock_guard<std::mutex> autoLock(extraDataLock_);
-    wantMap[COMMON_EVENT_ACTION_NAME] = want.GetAction();
-    OnDemandReasonExtraData extraData(data.GetCode(), data.GetData(), wantMap);
+    int64_t extraDataId = 0;
+    {
+        std::lock_guard<std::mutex> autoLock(extraDataLock_);
+        wantMap[COMMON_EVENT_ACTION_NAME] = want.GetAction();
+        OnDemandReasonExtraData extraData(data.GetCode(), data.GetData(), wantMap);
 
-    int64_t extraDataId = GenerateExtraDataIdLocked();
-    extraDatas_[extraDataId] = extraData;
-    HILOGD("CommonEventCollect save extraData %{public}d,n:%{public}zu",
-        static_cast<int32_t>(extraDataId), extraDatas_.size());
+        extraDataId = GenerateExtraDataIdLocked();
+        extraDatas_[extraDataId] = extraData;
+        HILOGD("CommonEventCollect save extraData %{public}d,n:%{public}zu",
+            static_cast<int32_t>(extraDataId), extraDatas_.size());
+    }
     if (workHandler_ == nullptr) {
         HILOGI("CommonEventCollect workHandler is nullptr");
         return -1;
