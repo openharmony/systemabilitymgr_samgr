7fed313b255a8e9d9a565567199503ffb8b3c3e1
GarboH <huangjiabao4@h-partners.com>
2025-09-23 16:19:52 +0800

    add trace, modify log
    Signed-off-by: GarboH <huangjiabao4@h-partners.com>

--- services/samgr/native/source/ability_death_recipient.cpp
+++ services/samgr/native/source/ability_death_recipient.cpp
@@ -18,46 +18,66 @@
 #include "iremote_proxy.h"
 #include "sam_log.h"
 #include "system_ability_manager.h"
+#include "ipc_skeleton.h"
+#include "hitrace_meter.h"
 
 namespace OHOS {
 void AbilityDeathRecipient::OnRemoteDied(const wptr<IRemoteObject>& remote)
 {
-    HILOGD("AbilityDeathRecipient OnRemoteDied called");
+    auto callPid = IPCSkeleton::GetCallingPid();
+    HILOGD("AbilityDeathRecipient OnRemoteDied called, pid:%{public}d", callingPid);
+    string OnRemoteDiedTag = ToString(callingPid) + "_AbilityDeathRecipient";
+    HitraceScopedEx samgrHitrace(HITRACE_LEVEL_INFO, HITRACE_TAG_SAMGR, OnRemoteDiedTag.c_str());
     SystemAbilityManager::GetInstance()->RemoveSystemAbility(remote.promote());
     HILOGD("AbilityDeathRecipients death notice success");
 }
 
 void SystemProcessDeathRecipient::OnRemoteDied(const wptr<IRemoteObject>& remote)
 {
-    HILOGD("SystemProcessDeathRecipient called!");
+    auto callPid = IPCSkeleton::GetCallingPid();
+    HILOGD("SystemProcessDeathRecipient called!, pid:%{public}d", callingPid);
+    string OnRemoteDiedTag = ToString(callingPid) + "_SystemProcessDeathRecipient";
+    HitraceScopedEx samgrHitrace(HITRACE_LEVEL_INFO, HITRACE_TAG_SAMGR, OnRemoteDiedTag.c_str());
     SystemAbilityManager::GetInstance()->RemoveSystemProcess(remote.promote());
     HILOGD("SystemProcessDeathRecipient death notice success");
 }
 
 void AbilityStatusDeathRecipient::OnRemoteDied(const wptr<IRemoteObject>& remote)
 {
-    HILOGD("AbilityStatusDeathRecipient called!");
+    auto callPid = IPCSkeleton::GetCallingPid();
+    HILOGD("AbilityStatusDeathRecipient called!, pid:%{public}d", callingPid);
+    string OnRemoteDiedTag = ToString(callingPid) + "_AbilityStatusDeathRecipient";
+    HitraceScopedEx samgrHitrace(HITRACE_LEVEL_INFO, HITRACE_TAG_SAMGR, OnRemoteDiedTag.c_str());
     SystemAbilityManager::GetInstance()->UnSubscribeSystemAbility(remote.promote());
     HILOGD("AbilityStatusDeathRecipient death notice success");
 }
 
 void AbilityCallbackDeathRecipient::OnRemoteDied(const wptr<IRemoteObject>& remote)
 {
-    HILOGD("AbilityCallbackDeathRecipient called!");
+    auto callPid = IPCSkeleton::GetCallingPid();
+    HILOGD("AbilityCallbackDeathRecipient called!, pid:%{public}d", callingPid);
+    string OnRemoteDiedTag = ToString(callingPid) + "_AbilityCallbackDeathRecipient";
+    HitraceScopedEx samgrHitrace(HITRACE_LEVEL_INFO, HITRACE_TAG_SAMGR, OnRemoteDiedTag.c_str());
     SystemAbilityManager::GetInstance()->OnAbilityCallbackDied(remote.promote());
     HILOGD("AbilityCallbackDeathRecipient death notice success");
 }
 
 void RemoteCallbackDeathRecipient::OnRemoteDied(const wptr<IRemoteObject>& remote)
 {
-    HILOGD("RemoteCallbackDeathRecipient called!");
+    auto callPid = IPCSkeleton::GetCallingPid();
+    HILOGD("RemoteCallbackDeathRecipient called!, pid:%{public}d", callingPid);
+    string OnRemoteDiedTag = ToString(callingPid) + "_RemoteCallbackDeathRecipient";
+    HitraceScopedEx samgrHitrace(HITRACE_LEVEL_INFO, HITRACE_TAG_SAMGR, OnRemoteDiedTag.c_str());
     SystemAbilityManager::GetInstance()->OnRemoteCallbackDied(remote.promote());
     HILOGD("RemoteCallbackDeathRecipient death notice success");
 }
 
 void SystemProcessListenerDeathRecipient::OnRemoteDied(const wptr<IRemoteObject>& remote)
 {
-    HILOGD("SystemProcessListenerDeathRecipient called!");
+    auto callPid = IPCSkeleton::GetCallingPid();
+    HILOGD("SystemProcessListenerDeathRecipient called!, pid:%{public}d", callingPid);
+    string OnRemoteDiedTag = ToString(callingPid) + "_SystemProcessListenerDeathRecipient";
+    HitraceScopedEx samgrHitrace(HITRACE_LEVEL_INFO, HITRACE_TAG_SAMGR, OnRemoteDiedTag.c_str());
     sptr<ISystemProcessStatusChange> listener = iface_cast<ISystemProcessStatusChange>(remote.promote());
     SystemAbilityManager::GetInstance()->UnSubscribeSystemProcess(listener);
     HILOGD("SystemProcessListenerDeathRecipient death notice success");
